d711442093362d7d8b06a0b86d5fe7d127a2fca6
Ian Chow <icho0012@student.monash.edu>
2024-06-05 03:11:07 +1000

    Added upload path in urls.py and edited home method and created image_upload_view method

--- webapp/website/urls.py
+++ webapp/website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('upload/', views.image_upload_view, name='upload'),
 ]
--- webapp/website/views.py
+++ webapp/website/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+import boto3
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # Fetch the uploaded image names from the S3 bucket
+    s3 = boto3.client('s3',
+                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                      region_name=settings.AWS_REGION_NAME)
+    bucket_name = settings.AWS_S3_BUCKET_NAME
+    image_names = [obj.key for obj in s3.list_objects(Bucket=bucket_name)['Contents']]
 
+    return render(request, 'home.html', {'image_names': image_names})
+
+
+def image_upload_view(request):
+    if request.method == 'POST' and request.FILES['image']:
+        image = request.FILES['image']
+        s3 = boto3.client('s3',
+                          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                          region_name=settings.AWS_REGION_NAME)
+
+        bucket_name = settings.AWS_S3_BUCKET_NAME
+        s3.upload_fileobj(image, bucket_name, image.name)
+
+        # Save the image information to the database
+        image_obj = Image.objects.create(file=image, name=image.name)
+        image_obj.save()
+
+        return HttpResponse('Image uploaded successfully.')
+    return render(request, 'upload.html')
