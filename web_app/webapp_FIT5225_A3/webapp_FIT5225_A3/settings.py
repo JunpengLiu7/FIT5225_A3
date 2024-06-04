f4ae555f43d210349a255f63bfadf2997313f9a4
Ian Chow <icho0012@student.monash.edu>
2024-06-04 18:30:32 +1000

    Removed unnecessary methods

--- web_app/webapp_FIT5225_A3/webapp_FIT5225_A3/settings.py
+++ web_app/webapp_FIT5225_A3/webapp_FIT5225_A3/settings.py
@@ -73,17 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'webapp_FIT5225_A3.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
