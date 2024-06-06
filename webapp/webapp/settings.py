d23f0ac8173c9891bfa5f8c8e1a82c0151c4d5be
Ian Chow <icho0012@student.monash.edu>
2024-06-06 14:02:52 +1000

    Added static URL, MEDIA_URL, MEDIA ROOT and Staticfiles_dirs

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -121,6 +121,7 @@ COGNITO_REGION = 'us-east-1'
 
 # AWS S3 Settings
 AWS_S3_BUCKET_NAME = '5225-a3-images-demo'
+AWS_REGION_NAME = 'us-east-1'
 
 # Django Storage Config
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
